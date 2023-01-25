ccb86243093f9f8a30a9775073fea6a42a557e88
Milkyway5155 <parprica0409@naver.com>
2023-01-26 01:30:03 +0900

    파이어베이스 연동 - 성공~~!!!!

--- Map Bucketlist/Map_BucketlistApp.swift
+++ Map Bucketlist/Map_BucketlistApp.swift
@@ -6,12 +6,32 @@
 //
 
 import SwiftUI
+import FirebaseCore
 
 @main
 struct Map_BucketlistApp: App {
+    
+    //@UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    init() {
+        FirebaseApp.configure()
+    }
     var body: some Scene {
-        WindowGroup {
-            ContentView()
+      WindowGroup {
+          IconView()
+
         }
     }
 }
+
+
+/*
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+
+    return true
+  }
+}
+ */
